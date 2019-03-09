12297f41cb49edebd236624d98b723b36583849e
Ted Romanus <ted@romanus.co>
2019-03-09 11:02:29 +0200

    weather-provider code refactoring

--- weather-provider/src/main/scala/ua/ucu/edu/Main.scala
+++ weather-provider/src/main/scala/ua/ucu/edu/Main.scala
@@ -15,7 +15,6 @@ import scala.util.{Failure, Success}
 object Main extends App {
 
   val logger = LoggerFactory.getLogger(getClass)
-  val APIKEY = "235343b13ac90be59884ca36d5325949"
 
   logger.info("======== Weather Provider App Init ========")
 
@@ -24,13 +23,8 @@ object Main extends App {
 
   import duration._
 
-  val BrokerList: String = System.getenv("KAFKA_BROKERS")
   val Topic = "weather_data"
-  val props = new Properties()
-  props.put("bootstrap.servers", BrokerList)
-  props.put("client.id", "weather-provider")
-  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  val props = Config.get_kafka_properties()
 
   logger.info("initializing producer")
 
@@ -47,13 +41,11 @@ object Main extends App {
       val lat = curr
       val long = curr
 
-      val responseFuture: Future[HttpResponse] = Http().singleRequest(
-        HttpRequest(uri = s"https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$long&&APPID=$APIKEY"))
-
-      responseFuture
-        .onComplete {
+      Http().singleRequest(
+        Weather.get_daily_request(lat, long)
+      ).onComplete {
           case Success(res:HttpResponse) => {
-            val message = lat + ";" + long + ";" + extract_data(res)
+            val message = lat + ";" + long + ";" + JSONProcessor.extract_data(res)
             val data = new ProducerRecord[String, String](Topic, message)
             producer.send(data, (metadata: RecordMetadata, exception: Exception) => {
               logger.trace(metadata.toString, exception)
@@ -64,18 +56,39 @@ object Main extends App {
     }
   })
 
-  def extract_data(response: HttpResponse):String = {
-    val str = response.entity.toString
-    val temp_base = str.indexOf("temp")
-    val temp_begin = str.indexOf(":", temp_base) + 1
-    val temp_end = str.indexOf(",", temp_begin)
-    val temp = str.substring(temp_begin, temp_end)
+  object JSONProcessor {
+    def extract_data(response: HttpResponse):String = {
+      val str = response.entity.toString
+      val temp_base = str.indexOf("temp")
+      val temp_begin = str.indexOf(":", temp_base) + 1
+      val temp_end = str.indexOf(",", temp_begin)
+      val temp = str.substring(temp_begin, temp_end)
 
-    val hum_base = str.indexOf("humidity")
-    val hum_begin = str.indexOf(":", hum_base) + 1
-    val hum_end = str.indexOf(",", hum_begin)
-    val hum = str.substring(hum_begin, hum_end)
+      val hum_base = str.indexOf("humidity")
+      val hum_begin = str.indexOf(":", hum_base) + 1
+      val hum_end = str.indexOf(",", hum_begin)
+      val hum = str.substring(hum_begin, hum_end)
 
-    temp + ";" + hum
+      temp + ";" + hum
+    }
+  }
+
+  object Config {
+
+    def get_kafka_properties():Properties = {
+      val props = new Properties()
+      props.put("bootstrap.servers", System.getenv("KAFKA_BROKERS"))
+      props.put("client.id", "weather-provider")
+      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+      props
+    }
+  }
+
+  object Weather {
+    val APIKEY = "235343b13ac90be59884ca36d5325949"
+
+    def get_daily_request(lat:Float, long:Float): HttpRequest =
+      HttpRequest(uri = s"https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&&APPID=${APIKEY}")
   }
 }
