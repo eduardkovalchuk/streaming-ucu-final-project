5a080ee4dac910e7dd76207d7f3956565759ae09
eduardkovalchuk <edward.kovalchuk11@gmail.com>
2019-03-09 23:13:31 +0200

    set app plants

--- solar-panel-emulator/src/main/scala/ua/ucu/edu/Main.scala
+++ solar-panel-emulator/src/main/scala/ua/ucu/edu/Main.scala
@@ -2,15 +2,13 @@ package ua.ucu.edu
 
 import scala.io.Source
 import akka.actor._
-//import com.fasterxml.jackson.module.scala.DefaultScalaModule
-//import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.databind.ObjectMapper
 import ua.ucu.edu.model.Location
 import ua.ucu.edu.actor.PlantManagerActor
 
 //import scala.collection.immutable
 //import ua.ucu.edu.kafka.DummyDataProducer
-
-
 
 
 object Main extends App {
@@ -19,27 +17,22 @@ object Main extends App {
 //
 //  DummyDataProducer.pushTestData()
 
-//  val panelSys = ActorSystem("Panel")
-
-//  panelSys.actorOf(Props(new PlantManagerActor(("someId"), Location(0,0))), "panelActor")
-
   // get data from json file with locations
-//  val locationsFile = Source.fromFile("locations.json").getLines.mkString
+  val locationsFile = Source.fromFile("locations.json").getLines.mkString
 
-//  val mapper = new ObjectMapper()
-//  mapper.registerModule(DefaultScalaModule)
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
 
-//  val locations: List[Map[String, Int]] = mapper.readValue(locationsFile, classOf[List[Map[String, Int]]])
-
+  val locations: List[Map[String, Int]] = mapper.readValue(locationsFile, classOf[List[Map[String, Int]]])
 
   // initialize actor System
   val actorSys = ActorSystem("Solar-plants-actors")
-  actorSys.actorOf(Props(new PlantManagerActor("name", Location(0.0,0.0) )), "somename")
 
-//  for (locData <- locations) {
-//    val location = Location(locData("latitude").toDouble, locData("longitude").toDouble)
-//    val plantId = locData("id")
 
-//    actorSys.actorOf(Props(new PlantManagerActor(s"plant-$plantId", location)))
-//  }
+  for (locData <- locations) {
+    val location = Location(locData("latitude").toDouble, locData("longitude").toDouble)
+    val plantId = locData("id")
+
+    actorSys.actorOf(Props(new PlantManagerActor(s"plant-$plantId", location)))
+  }
 }
